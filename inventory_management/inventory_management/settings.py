979ddfcc6749b93b76117285bd3463d3af7ca5d9
kcurt123 <kurt.sama@gmail.com>
2024-03-28 18:45:55 -0500

    edited admin page title, added link to return to dashboard

--- inventory_management/inventory_management/settings.py
+++ inventory_management/inventory_management/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = "inventory_management.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "inventory/templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
