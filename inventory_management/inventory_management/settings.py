470208a9075d4b0832bf9587fddd931568b5606b
kcurt123 <kurt.sama@gmail.com>
2024-05-08 19:10:42 -0500

    initial commit

--- inventory_management/inventory_management/settings.py
+++ inventory_management/inventory_management/settings.py
@@ -79,16 +79,16 @@ WSGI_APPLICATION = "inventory_management.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-#DATABASES = {
-#    'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
-#}
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.parse(os.getenv('DATABASE_URL'))
 }
+
+#DATABASES = {
+#    "default": {
+#        "ENGINE": "django.db.backends.sqlite3",
+#        "NAME": BASE_DIR / "db.sqlite3",
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
